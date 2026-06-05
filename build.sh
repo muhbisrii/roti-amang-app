91184b6936f16bd215d34b18d158c85a3010f26d
Muh Bisri <m.bisri139@gmail.com>
2026-06-05 12:03:22 +0800

    Perbaikan skrip build vercel

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-1. Vercel akan mendownload mesin Flutter
-
 git clone https://github.com/flutter/flutter.git -b stable
 export PATH="$PATH:pwd/flutter/bin"
-
-2. Vercel akan mem-build aplikasinya jadi Website
-
 flutter build web --release
