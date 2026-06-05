0c98b5c7ddf543b93818b2b920200cbdb7b29418
Muh Bisri <m.bisri139@gmail.com>
2026-06-05 12:16:02 +0800

    Fix build.sh script for Vercel

--- build.sh
+++ build.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-Download Flutter versi terbaru saja (sangat cepat)
-
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:pwd/flutter/bin"
-
-Matikan analitik agar proses tidak tertahan persetujuan
-
 flutter config --no-analytics
-
-Mulai proses pembuatan website
-
 flutter build web --release
