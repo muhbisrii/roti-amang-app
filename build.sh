9bd0883d3122592cbd41200f301fcd6407868b4c
Muh Bisri <m.bisri139@gmail.com>
2026-06-05 12:09:01 +0800

    Fix Vercel clone depth

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-git clone https://github.com/flutter/flutter.git -b stable
+
+Download Flutter versi terbaru saja (sangat cepat)
+
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:pwd/flutter/bin"
+
+Matikan analitik agar proses tidak tertahan persetujuan
+
+flutter config --no-analytics
+
+Mulai proses pembuatan website
+
 flutter build web --release
